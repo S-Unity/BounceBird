4965fd81a847fc7a137bfcab545581e06c5700bc
Zomato <sawaistar@gmaoil.com>
2020-05-31 22:30:46 +0530

    first commit.

---
+++ Assets/scripts/HighscoreText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class HighscoreText : MonoBehaviour
+{
+    Text highscore;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        highscore = GetComponent<Text>();
+        highscore.text = "High Score: " + PlayerPrefs.GetInt("HighScore").ToString();
+    }
+}
